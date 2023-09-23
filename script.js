811faaf0d94ad0c3776830cf70856c65b3400d35
qmkDuran <duran122410@gmail.com>
2023-09-23 13:11:23 -0400

    Second Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Solution: Pair with Target Sum
+
+function pair_with_target_sum(arr, targetSum) {
+  let left = 0,
+    right = arr.length - 1;
+  while (left < right) {
+    const currentSum = arr[left] + arr[right];
+    if (currentSum === targetSum) {
+      return [left, right];
+    }
+
+    if (targetSum > currentSum) {
+      left += 1; // we need a pair with a bigger sum
+    } else {
+      right -= 1; // we need a pair with a smaller sum
+    }
+  }
+  return [-1, -1];
+}
+
+console.log(pair_with_target_sum([], 6));
+console.log(pair_with_target_sum([2, 5, 9, 11], 11));
