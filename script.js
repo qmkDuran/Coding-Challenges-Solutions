d981f83b3869464b949cc010769de8bc9f3a58ee
qmkDuran <duran122410@gmail.com>
2023-09-24 17:00:56 -0400

    more code

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-// Solution: Pair with Target Sum
+// // Solution: Pair with Target Sum
 
-function pair_with_target_sum(arr, targetSum) {
-  let left = 0,
-    right = arr.length - 1;
-  while (left < right) {
-    const currentSum = arr[left] + arr[right];
-    if (currentSum === targetSum) {
-      return [left, right];
-    }
+// function pair_with_target_sum(arr, targetSum) {
+//   let left = 0,
+//     right = arr.length - 1;
+//   while (left < right) {
+//     const currentSum = arr[left] + arr[right];
+//     if (currentSum === targetSum) {
+//       return [left, right];
+//     }
 
-    if (targetSum > currentSum) {
-      left += 1; // we need a pair with a bigger sum
-    } else {
-      right -= 1; // we need a pair with a smaller sum
+//     if (targetSum > currentSum) {
+//       left += 1; // we need a pair with a bigger sum
+//     } else {
+//       right -= 1; // we need a pair with a smaller sum
+//     }
+//   }
+//   return [-1, -1];
+// }
+
+// console.log(pair_with_target_sum([], 6));
+// console.log(pair_with_target_sum([2, 5, 9, 11], 11));
+
+// Solution: Find Non-Duplicate Number Instances
+
+function remove_duplicates(arr) {
+  // index of the next non-duplicate element
+  let nextNonDuplicate = 1;
+
+  let i = 0;
+  while (i < arr.length) {
+    if (arr[nextNonDuplicate - 1] !== arr[i]) {
+      arr[nextNonDuplicate] = arr[i];
+      nextNonDuplicate += 1;
     }
+    i += 1;
   }
-  return [-1, -1];
+
+  return nextNonDuplicate;
 }
 
-console.log(pair_with_target_sum([], 6));
-console.log(pair_with_target_sum([2, 5, 9, 11], 11));
+console.log(remove_duplicates([2, 3, 3, 3, 6, 9, 9]));
+console.log(remove_duplicates([2, 2, 2, 11]));
