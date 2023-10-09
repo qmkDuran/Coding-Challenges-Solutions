f4468dd9fc20cb0b374a4b49b5226614addb22f7
qmkDuran <duran122410@gmail.com>
2023-10-09 14:35:52 -0400

    new solution

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+// s = "()";
+// s = "()[]{}";
+// s = "(]";
+
+//make a variable called stack equal to an empty array.
+//make a variable called map and add key and values based on the charaters
+//create a loop that intereated over each character in a string
+//within the loop create two if statements
+//first one check if the the characters fromt he string are in the map
+//if not add the character to the stack and then do continue so that the loop
+//restarts and looks at the necxt character in the string and if that charcacter is the map
+//go to the next if statement
+//the next if statement check to see if the character inside the stack matches any inside the map
+//if it does then remove the character from the stack and then the loop ends and returns true
+//if the stack is equal to 0
+//if the the loop ends and the stack is not equal to 0 then return false
+
 var isValid = function (s) {
-  const stack = []  
+  const stack = [];
   const map = {
     "}": "{",
     "]": "[",
@@ -7,12 +24,11 @@ var isValid = function (s) {
   };
 
   for (const char of s) {
-    /* Time O(N) */
     const isBracket = char in map;
     if (!isBracket) {
       stack.push(char);
       continue;
-    } /* Space O(N) */
+    }
 
     const isEqual = stack[stack.length - 1] === map[char];
     if (isEqual) {
