5d5537e4b361a779e3cffc891b1a9afc5f868d2c
qmkDuran <duran122410@gmail.com>
2023-10-16 17:37:15 -0400

    new solution

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-var hasCycle = function (head) {
-  let [slow, fast] = [head, head];
+const mostFrequentChar = (s) => {
+  const count = {}; // Create an empty object to keep track of character frequencies
 
-  while (fast && fast.next) {
-    /* Time O(N) */
-    slow = slow.next;
-    fast = fast.next.next;
-
-    const hasCycle = slow === fast;
-    if (hasCycle) return true;
+  // First loop to populate the count object
+  for (let char of s) {
+    if (!(char in count)) {
+      // Check if the character is not already in the count object
+      count[char] = 0; // Initialize the count for this character to 0
+    }
+    count[char] += 1; // Increment the count for this character
   }
 
-  return false;
+  let best = null; // Variable to store the most frequent character; initialized to null
+
+  // Second loop to find the most frequent character
+  for (let char of s) {
+    // If best is null or if the count of the current character is greater than the count of 'best'
+    if (best === null || count[char] > count[best]) {
+      best = char; // Update 'best' to the current character
+    }
+  }
+  return best; // Return the most frequent character
 };
 
-hasCycle()
+mostFrequentChar("bookeeper");
