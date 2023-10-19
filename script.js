0e09894e9ca1de921e387823f6cead555138b379
qmkDuran <duran122410@gmail.com>
2023-10-19 14:17:50 -0400

    new solution

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-const pairSum = (numbers, targetSum) => {
-  let sortedNums = numbers.sort();
-  let left = 0;
-  let right = numbers.length - 1;
+const zipperLists = (head1, head2) => {
+  const head = head1;
+  let tail = head;
+  let current1 = head1.next;
+  let current2 = head2;
+  let count = 0;
 
-  while (left < right) {
-    const sum = sortedNums[left] + sortedNums[right];
-    if (sum === targetSum) {
-      return [sortedNums[left], sortedNums[right]];
-    } else if (sum < targetSum) {
-      left += 1;
+  while (current1 !== null && current2 !== null) {
+    if (count % 2 === 0) {
+      tail.next = current2;
+      current2 = current2.next;
     } else {
-      right -= 1;
+      tail.next = current1;
+      current1 = current1.next;
     }
+    tail = tail.next;
+    count += 1;
   }
-};
 
-pairSum([3, 2, 5, 4, 1], 8);
+  if (current1 !== null) tail.next = current1;
+  if (current2 !== null) tail.next = current2;
+
+  return head;
+};
