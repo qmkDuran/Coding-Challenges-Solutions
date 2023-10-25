43a27a671c5ab2c775b53b9d57f3d4ef4faf14a5
qmkDuran <duran122410@gmail.com>
2023-10-25 14:34:05 -0400

    new solution

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
-  }
-}
-
-const a = new Node("a");
-const b = new Node("b");
-const c = new Node("c");
-const d = new Node("d");
-const e = new Node("e");
-const f = new Node("f");
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(4);
+const d = new Node(4);
+const e = new Node(-2);
+const f = new Node(1);
 
 a.left = b;
 a.right = c;
@@ -19,77 +11,15 @@ b.left = d;
 b.right = e;
 c.right = f;
 
-const depthFirstValues = (root) => {
-  if (root === null) return [];
+//       3
+//    /    \
+//   11     4
+//  / \      \
+// 4   -2     1
+const treeSum = (root) => {
+  if (root === null) return 0;
 
-  const values = [];
-  const stack = [root];
-
-  while (stack.length > 0) {
-    const node = stack.pop();
-    values.push(node.val);
-
-    if (node.right !== null) stack.push(node.right);
-
-    if (node.left !== null) stack.push(node.left);
-  }
-
-  return values;
+  return root.val + treeSum(root.left) + treeSum(root.right);
 };
 
-//      a
-//    /   \
-//   b     c
-//  / \     \
-// d   e     f
-
-depthFirstValues(a);
-//    -> ['a', 'b', 'd', 'e', 'c', 'f']
-
-Initialization:
-
-// EXPLANATION
-
-// The tree starts with the root node a.
-// The values array is initialized as an empty array.
-// The stack is initialized with the root node a.
-// First Iteration:
-
-// stack: [a]
-// The last node in the stack (a) is popped and its value is added to the values array.
-// The right child of a (c) is pushed onto the stack, followed by the left child (b).
-// values: [a]
-// Second Iteration:
-
-// stack: [c, b]
-// The last node in the stack (b) is popped and its value is added to the values array.
-// The right child of b (e) is pushed onto the stack, followed by the left child (d).
-// values: [a, b]
-// Third Iteration:
-
-// stack: [c, e, d]
-// The last node in the stack (d) is popped and its value is added to the values array.
-// d has no children, so the stack remains unchanged.
-// values: [a, b, d]
-// Fourth Iteration:
-
-// stack: [c, e]
-// The last node in the stack (e) is popped and its value is added to the values array.
-// e has no children, so the stack remains unchanged.
-// values: [a, b, d, e]
-// Fifth Iteration:
-
-// stack: [c]
-// The last node in the stack (c) is popped and its value is added to the values array.
-// The right child of c (f) is pushed onto the stack. c has no left child.
-// values: [a, b, d, e, c]
-// Sixth Iteration:
-
-// stack: [f]
-// The last node in the stack (f) is popped and its value is added to the values array.
-// f has no children, so the stack remains unchanged.
-// values: [a, b, d, e, c, f]
-// End of Traversal:
-
-// The stack is now empty, so the while loop ends.
-// The function returns the values array: [a, b, d, e, c, f].
+treeSum(a); // -> 21
